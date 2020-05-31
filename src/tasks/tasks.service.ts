c61e3abee9e1efa4e17a02e565406b7c7d530006
darisjerez <darisjerez@gmail.com>
2020-05-31 14:21:20 -0400

    initial commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from '../../dist/tasks/task.model';
+
+@Injectable()
+export class TasksService {
+   private tasks:Task[] = [];
+
+   getAllTasks(): Task[] {
+       return this.tasks;
+   }
+}
