3b61d98bd63923fc4197bfa22f114f0dc0489034
darisjerez <darisjerez@gmail.com>
2020-06-06 10:38:08 -0400

    add validator for empty values and add ESlint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -21,6 +21,8 @@ export class TasksController {
        return this.tasksService.getTaskById(id);
     }
     @Post()
+
+    @UsePipes(ValidationPipe)
     createTask(
        @Body() createTaskDto: CreateTaskDto
     ): Task{
