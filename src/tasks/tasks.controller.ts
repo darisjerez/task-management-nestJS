6cd8f2a3e6a2ee81bc85724764c3fe905ea847a7
darisjerez <darisjerez@gmail.com>
2020-06-06 10:09:01 -0400

    add ESlint package and refactor getAllTasks function to include filtering

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService:TasksService){}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getAllTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilters(filterDto);
+        }else{
+            return this.tasksService.getAllTasks();
+        } 
     }
-
     @Get('/:id')
     getTask(@Param('id') id: string): Task{
        return this.tasksService.getTaskById(id);
@@ -27,7 +31,6 @@ export class TasksController {
         this.tasksService.deleteTaskById(id);
         return "Task Removed";
     }
-
     @Patch('/:id/status')
     updateTask(
         @Param('id') id: string,
