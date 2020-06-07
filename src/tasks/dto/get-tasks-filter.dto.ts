5392af5651b4facc40c21b66d9c0cb54b6581577
darisjerez <darisjerez@gmail.com>
2020-06-07 15:18:20 -0400

    add validation for empty search

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,14 @@
 import { TaskStatus } from '../task.model';
+import { IsOptional, IsNotEmpty, IsIn } from 'class-validator';
+
+
+
 export class GetTasksFilterDto{
+    @IsOptional()
+    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
     status: TaskStatus;
+
+    @IsOptional()
+    @IsNotEmpty()
     search: string
 }
---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,30 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatutes = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE
+    ];
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    transform(value: any){
+
+        value = value.toUpperCase();
+
+        if(!this.isStatusValid(value)){
+            throw new BadRequestException(`${value} is not a valid status for the task`)
+        }
+        
+        return value;
+    }
+   
+
+    private isStatusValid(status: any){
+        const indexStatus = this.allowedStatutes.indexOf(status);
+        return ~indexStatus !== 0
+
+    }
+    
+    
+}
