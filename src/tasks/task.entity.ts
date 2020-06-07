3f2b4cec809c2512bd3ea887cd9578b3cba660c7
darisjerez <darisjerez@gmail.com>
2020-06-07 15:55:18 -0400

    add entity for task

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,18 @@
+import { BaseEntity, PrimaryGeneratedColumn, Entity, Column } from "typeorm";
+import { TaskStatus } from "./task.model";
+
+@Entity()
+export class TaskEntity extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: TaskStatus;
+}
