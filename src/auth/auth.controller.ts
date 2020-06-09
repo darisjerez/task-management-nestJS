2439b291039fd3cdace06718d7efe1b821d1b7d4
darisjerez <darisjerez@gmail.com>
2020-06-09 19:38:00 -0400

    add auth module and signup method

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Post('/signup')
+    signUp(
+        @Body() authCredentialsDto: AuthCredentialsDto
+    ){
+       this.authService.signUp(authCredentialsDto);
+    }
+}
