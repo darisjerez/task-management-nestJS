fa51d7e16ef4220c48e790ce985fd1275807146b
darisjerez <darisjerez@gmail.com>
2020-06-11 13:52:06 -0400

    add encrypting for password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,6 +3,7 @@ import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,18 +11,21 @@ export class UserRepository extends Repository<User> {
         const { username, password } = authCredentialsDto;
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
          
         try {
             await user.save();
         } catch (error) {
             if (error.code == "23505") {
-                throw new ConflictException("User Already Exists!");
-                
+                throw new ConflictException("User Already Exists!");  
             } else {
                 throw new InternalServerErrorException();
-                
             }
         }
+    }
+    
+    private async hashPassword(password: string, salt:string){
+        return bcrypt.hash(password, salt);
     }
 }
